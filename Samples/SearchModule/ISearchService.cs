97bd4158ce5da3b21c1db807fca6896b7886ff71
Lily05 <12858661+lily05@user.noreply.gitee.com>
2025-05-28 17:37:51 +0800

    [FEAT] filter dulplicated results and add ai settings

--- Samples/SearchModule/ISearchService.cs
+++ Samples/SearchModule/ISearchService.cs
@@ -3,9 +3,7 @@ using Quantum.Sdk.Abstractions;
 namespace SearchModule;
 public interface ISearchService : IInitializableService // 可以继承 Quantum.Sdk 中的接口
 {
-    public Task<List<(string title, string url, string date, int number)>> getSearchResult(string searchText);
-    
-    public void setEnableAI(bool enableAI);
+    public Task<List<(string title, string url, string date, int number, string source)>> getSearchResult(string searchText);
     public int sourceCnt();
     public int pingSource(int source);
     public void setEnableSource(int source, bool enable);
--- Samples/SearchModule/ISourceRetriever.cs
+++ Samples/SearchModule/ISourceRetriever.cs
@@ -8,5 +8,5 @@ public interface ISourceRetriever
     string SourceName { get; }
     string SourceUrl { get; }
 
-    Task<List<(string title, string url, string date, int number)>> RetrieveSourcesAsync(int numPosts = 10, string keyWord = "");
+    Task<List<(string title, string url, string date, int number)>> RetrieveSourcesAsync(int numPosts = 30, string keyWord = "");
 }
